3f29bf42518c61d878c54ce59517abcf78c603d9
josephsmithberger <josephsmithberger@gmail.com>
2025-12-23 19:42:53 -0500

    Player handling

--- scripts/character_creator_controller.gd
+++ scripts/character_creator_controller.gd
@@ -2,9 +2,26 @@ extends Control
 
 @onready var camera_feed = $HBoxContainer/VBoxContainer/SubViewportContainer/SubViewport/CameraFeed
 @onready var character_preview = $HBoxContainer/SubViewportContainer/SubViewport/character_preview
+@onready var player_input = $PanelContainer/MarginContainer/VBoxContainer/player_name
+
+var current_image: Image
 
 func _ready():
 	camera_feed.photo_taken.connect(_on_photo_taken)
 
 func _on_photo_taken(image: Image):
+	current_image = image
 	character_preview.confirm(image)
+
+
+func _on_confirm_name_pressed() -> void:
+	Global.player_data[player_input.text] = current_image
+	get_tree().change_scene_to_file("res://scenes/title_screen.tscn")
+
+func _on_player_name_text_changed(new_text: String) -> void:
+	if $PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/confirm_name.disabled:
+		$PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/confirm_name.disabled = false
+
+func _on_retake_photo_pressed() -> void:
+	$HBoxContainer/SubViewportContainer/SubViewport/character_preview._play_idle()
+	$AnimationPlayer.play("retake")
--- scripts/title_screen.gd
+++ scripts/title_screen.gd
@@ -1,4 +1,22 @@
 extends Control
+@onready var party_member = $VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer/party_member
+@onready var party_container = $VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/HBoxContainer
+
+func _ready() -> void:
+	if Global.player_data.size() == 0:
+		$VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/start_game.disabled = true
+		$VBoxContainer/PanelContainer/MarginContainer/VBoxContainer/Label.show()
+	# Show and populate party members based on player data
+	for i in Global.player_data.size():
+		var member: TextureRect
+		if i == 0:
+			# Use the existing party_member node for the first member
+			member = party_member
+		else:
+			# Duplicate for additional members
+			member = party_member.duplicate()
+			party_container.add_child(member)
+		member.show()
 
 func _on_add_to_party_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/character_creator.tscn")
